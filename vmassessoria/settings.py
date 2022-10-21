e0ec8604dfff19e48ff18f0804a5b6cb41369296
mod64bits <mod64bits@gmail.com>
2022-10-21 13:21:49 -0300

    s3

--- vmassessoria/settings.py
+++ vmassessoria/settings.py
@@ -159,6 +159,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
+    
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
@@ -168,3 +169,4 @@ if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
     AWS_S3_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_S3_ADDRESSING_STYLE = "virtual"
+    AWS_LOCATION = 'static'
