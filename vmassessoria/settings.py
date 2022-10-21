5907ce13a9892c727ab7b8e611817ea0608cf7cf
mod64bits <mod64bits@gmail.com>
2022-10-21 09:27:57 -0300

    updates for eb

--- vmassessoria/settings.py
+++ vmassessoria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-t_z6a+7@58d^6s*r_u78lud!wf1a4c_cffcztc-*9x64-&@e!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
