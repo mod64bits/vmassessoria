9649421af217b8df859c1bd1ff5b4622b7ce205d
mod64bits <mod64bits@gmail.com>
2022-10-21 14:54:46 -0300

    s3 static

--- vmassessoria/settings.py
+++ vmassessoria/settings.py
@@ -139,11 +139,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'static'
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
