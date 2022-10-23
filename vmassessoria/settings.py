4f48495ce0f26c23d7c190402c7de050321fedd5
mod64bits <mod64bits@gmail.com>
2022-10-22 21:57:51 -0300

    s3 static

--- vmassessoria/settings.py
+++ vmassessoria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-t_z6a+7@58d^6s*r_u78lud!wf1a4c_cffcztc-*9x64-&@e!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vmassessoria-dev.us-west-2.elasticbeanstalk.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
     "cloudinary",
     # libs
-    "storages",
+
     # apps
     "apps.home",
 
