792112df1397b3ae3114d2ae2a5eb79a52849d61
mod64bits <mod64bits@gmail.com>
2022-10-24 19:39:19 -0300

    configuração sratics files

--- vmassessoria/urls.py
+++ vmassessoria/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from apps.home.views import HomeView
 urlpatterns = [
+    path("", HomeView.as_view()),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
