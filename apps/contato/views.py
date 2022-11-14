916bf34fd70929f0bd7b344e6e20e5ff38a5d737
mod64bits <mod64bits@gmail.com>
2022-11-14 15:53:42 -0300

    Servidor de envio de email

--- apps/contato/views.py
+++ apps/contato/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponseRedirect
+
 from .forms import ContatoForm
 from django.views.generic.edit import CreateView
 from .models import Contato
 from apps.core.send_email import SendVmMail
+import requests
 
 class ContatoView(CreateView):
     template_name = 'contato/contato.html'
@@ -9,9 +12,10 @@ class ContatoView(CreateView):
     model = Contato
     success_url = '/contato/'
 
+
     def form_valid(self, form):
         """If the form is valid, send an email and then save the form."""
         print()
         MailSend = SendVmMail(subject="test", nome="test", assunto="tesdt" ,telefone="199996647569", mensagem="mensagem")
-        MailSend.contato(email="contato@vmassessoria.tech")
-        return super(ContatoForm, self).form_valid(form)
+        MailSend.contato(email="marciooliveiradedeus@gmail.com")
+        return HttpResponseRedirect('/contato/')
